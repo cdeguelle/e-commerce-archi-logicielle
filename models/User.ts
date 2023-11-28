8380efd03bc8ab81cf850eb8f9d8ce6aaadc4b3b
ClementObspher <144014423+ClementObspher@users.noreply.github.com>
2023-11-28 14:19:13 +0100

    :sparkles: user controller

--- models/User.ts
+++ models/User.ts
@@ -1,6 +1,6 @@
-import { Schema, model, Document } from "mongoose"
+import { Schema, model } from "mongoose"
 
-interface User {
+export interface User {
 	id: string
 	email: string
 	password: string
